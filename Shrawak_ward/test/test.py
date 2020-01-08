3a254fd10cf29852efb080c4c50e28c4173f4684
ShrawakB <sb02431318@student.ku.edu.np>
2020-01-08 18:49:47 +0545

    previously tested how to change windows in pyqt

---
+++ Shrawak_ward/test/test.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from win import Ui_OtherWindow
+
+class Ui_MainWindow(object):
+    def openWindow(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_OtherWindow()
+        self.ui.setupUi(self.window)
+        MainWindow.hide()
+        self.window.show()
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 57)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(170, 170, 401, 171))
+        self.pushButton.setStyleSheet("font: 16pt \"MS Shell Dlg 2\";")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.openWindow)
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(130, 60, 501, 81))
+        self.textBrowser.setStyleSheet("font: 22pt \"MS Shell Dlg 2\";")
+        self.textBrowser.setObjectName("textBrowser")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Next window"))
+        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:22pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:26pt; font-weight:600;\">       MAIN WINDOW</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ Shrawak_ward/test/win.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'win.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_OtherWindow(object):
+    def setupUi(self, OtherWindow):
+        OtherWindow.setObjectName("OtherWindow")
+        OtherWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(OtherWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(200, 120, 491, 61))
+        self.label.setStyleSheet("font: 75 26pt \"MS Shell Dlg 2\";")
+        self.label.setObjectName("label")
+        OtherWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(OtherWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        OtherWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(OtherWindow)
+        self.statusbar.setObjectName("statusbar")
+        OtherWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(OtherWindow)
+        QtCore.QMetaObject.connectSlotsByName(OtherWindow)
+
+    def retranslateUi(self, OtherWindow):
+        _translate = QtCore.QCoreApplication.translate
+        OtherWindow.setWindowTitle(_translate("OtherWindow", "MainWindow"))
+        self.label.setText(_translate("OtherWindow", "WELCOME TO NEXT WINDOW"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    OtherWindow = QtWidgets.QMainWindow()
+    ui = Ui_OtherWindow()
+    ui.setupUi(OtherWindow)
+    OtherWindow.show()
+    sys.exit(app.exec_())
