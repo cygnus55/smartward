ed5a9e0f2bb269b65972e80afc5ef92c125d1e14
RamrajCh <ch.ramraj35@gmail.com>
2020-02-27 09:38:05 +0545

    Birth Certificate Prototype Complete

--- Smartward/forms/sw_pdf.py
+++ Smartward/forms/sw_pdf.py
@@ -1,4 +1,9 @@
+import pickle
 from fpdf import FPDF
+
+ward=open("ward.pickle",'rb')
+wardinfo=pickle.load(ward)
+municipality,address=wardinfo['municipality'],wardinfo['address']
 
 logo="logo.png"
 
@@ -7,7 +12,6 @@ class PDF():
         #self.logopath=logopath
         self.pdf=FPDF()
         self.pdf.add_page()
-        self.addLogo(logo)
         self.addHeader()
         #self.pdf.output(pdffilepath)
 
@@ -15,6 +19,7 @@ class PDF():
         self.pdf.image(imgpath,15,30,25,24.67)
 
     def addHeader(self):
+        self.addLogo(logo)
         self.pdf.set_font("Times",size=10)
         self.pdf.cell(180, 8, txt="Schedule-12/13/14/15", ln=1, align='C')
         self.pdf.cell(180, 8, txt="(Related with Rule 7)", ln=1, align='C')
@@ -22,23 +27,45 @@ class PDF():
         self.pdf.cell(180, 8, txt="Ministry of Federal Affairs and Local Development", ln=1, align='C')
         self.pdf.set_font("Times",'B',size=10)
         self.pdf.cell(180,8,txt="Office Of Local Registrar",ln=1,align='C')
+        self.setOfficeAddress(municipality,address)
 
-    def setOfficeAddress(self, municipality, district):
+    def setOfficeAddress(self, municipality, address):
         self.pdf.set_font("Times",size=10)
         self.pdf.cell(180,8,txt=municipality,ln=1,align='C')
-        self.pdf.cell(180,8,txt=district,ln=1,align="C")
+        self.pdf.cell(180,8,txt=address,ln=1,align="C")
 
 
 
 class BirthCertificate(PDF):
     def __init__(self):
         super().__init__()
-        self.pdf.set_font("Times",'BU',10)
-        self.pdf.cell(180,8,txt='Birth Certificate',ln=1,align='C')
+        self.pdf.set_font("Times",'BU',20)
+        self.pdf.cell(180,8,txt='Birth Registration Certificate',ln=1,align='C')
+        self.setBody('12321','2056/09/03','4233','Ram Hari Khatiwada','Hari Krishna Ghimere','Tilak Ghimere','Sunita Ghimere','Ghana Shyam Ghimere','05','Namobuddha','2056/01/03','1997/05/09','Methinkot Hospital','2023/05/06','Kavre','23452','2030/06/09','Kavre','875683')
+        #self.getfromtxt('birthcertificate.txt')
+        #self.setBody('fdsf')
+
+    def getfromtxt(self,txt):
+        content=''
+        text=open(txt,'r')
+        txt=text.readlines()
+        return txt
+
+    def setBody(self,*args):
+        print(args)
+        contents=self.getfromtxt('birthcertificate.txt')
+        #args=('12321','2056/09/03','4233','Ram Hari Khatiwada','Hari Krishna Ghimere','Tilak Ghimere','Sunita Ghimere','Ghana Shyam Ghimere','05','Namobuddha','2056/01/03','1997/05/09','Methinkot Hospital','2023/05/06','Kavre','23452','2030/06/09','Kavre','875683')
+        #contents=content.format('12321','2056/09/03','4233','Ram Hari Khatiwada','Hari Krishna Ghimere','Tilak Ghimere','Sunita Ghimere','Ghana Shyam Ghimere','05','Namobuddha','2056/01/03','1997/05/09','Methinkot Hospital','2023/05/06','Kavre','23452','2030/06/09','Kavre','875683')
+        lines=[]
+        for content in contents:
+            lines.append(content.format(*args))
+        for line in lines:
+            self.pdf.set_font('Times',size=10)
+            self.pdf.cell(1950,8,txt=line,ln=1,align='L')
 
     def output(self,pdffilepath):
         self.pdf.output(pdffilepath)
 
 a=BirthCertificate()
-a.setOfficeAddress('Namobuddha Municipality','Kavre')
+#a.setOfficeAddress('Namobuddha Municipality','Kavre')
 a.output("test.pdf")
